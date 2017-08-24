219149f89aa2cd8b201161284e083aea579881ff
Echo <yhyecho@gmail.com>
2017-08-24 19:05:57 +0800

    vue-cli env

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+</style>
